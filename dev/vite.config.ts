4b8b7d70332cd5d71dccba4e49597353569096fd
Yrobot <Yrobot@qq.com>
2023-03-05 16:39:31 +0800

    init vite + basic sw

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -1,22 +1,22 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nativeSW } from "vite-plugin-native-sw";
 
 export default defineConfig({
   root: "dev",
   build: {
     outDir: "../dist",
-    rollupOptions: {
-      input: {
-        app: resolve(__dirname, "index.html"),
-        "sw-proxy": resolve(__dirname, `sw-proxy.ts`),
-      },
-      output: {
-        entryFileNames: ({ name }) =>
-          name === "sw-proxy" ? "[name].js" : "assets/[name]-[hash].js",
-      },
-    },
   },
-  server: {},
-  plugins: [tsconfigPaths()],
+  plugins: [
+    tsconfigPaths(),
+    nativeSW({
+      entries: [
+        {
+          src: resolve(__dirname, "sw-proxy.ts"),
+          dist: "sw-proxy.js",
+        },
+      ],
+    }),
+  ],
 });
